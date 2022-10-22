81f47d3f31e8c75d56bd09827caa003f4af2b7cb
robAnderson <reanderson89@gmail.com>
2022-10-21 17:48:17 -0700

    switched to HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Portfolio from "./pages/portfolio/Portfolio";
 import Nav from "./components/Nav/Nav";
 import About from "./pages/about/About.jsx";
 import Contact from "./pages/contact/Contact.jsx";
-import { Route, BrowserRouter, Routes} from "react-router-dom";
+import { Route, BrowserRouter, Routes, HashRouter} from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./utils/theme.jsx";
 import CssBaseLine from "@material-ui/core/CssBaseline";
@@ -14,15 +14,15 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseLine />
-    <BrowserRouter basename="/robert-anderson">
+    <HashRouter >
     <Nav color={theme}/>
     <Routes>
+      <Route path="/portfolio" element={<Portfolio />} color={theme} />
+      <Route path="/about" element={<About />} color={theme}/>
+      <Route path="/contact" element={<Contact />} />
       <Route path="/" element={<Portfolio />}/>
-      <Route exact path="/portfolio" element={<Portfolio />} color={theme} />
-      <Route exact path="/about" element={<About />} color={theme}/>
-      <Route exact path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </ThemeProvider>
   );
 }
